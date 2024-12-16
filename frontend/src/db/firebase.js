3abd690df09f87dd2825d838c45d6d8c6a2a5258
Malidu Pahasara <malidupahasara04@gmail.com>
2024-12-17 00:31:03 +0530

    Update firebase.js

--- frontend/src/db/firebase.js
+++ frontend/src/db/firebase.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAAIa-8rd9waQgcmtToumz1f4VsJcPRZ-0",
-  authDomain: "paf-new-b0436.firebaseapp.com",
-  projectId: "paf-new-b0436",
-  storageBucket: "paf-new-b0436.firebasestorage.app",
-  messagingSenderId: "134472351141",
-  appId: "1:134472351141:web:2eda386e239f4cadeb7c4c",
+ 
 };
  
 // Initialize Firebase
